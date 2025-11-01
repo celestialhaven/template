7a6d459de66e432d894e6ad65e016c2715ddcb8d
celestialhaven <jonzmabz04@gmail.com>
2025-11-01 19:14:36 +0800

    week01

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)

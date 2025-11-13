b45206bf3cdc4606fac54ed964acd72404ca950e
celestialhaven <jonzmabz04@gmail.com>
2025-11-13 23:03:53 +0800

    week02 update

---
+++ database/assignment2.sql
@@ -0,0 +1,5 @@
+-- 1) Insert Tony Stark into the account table
+INSERT INTO public.account
+  (account_firstname, account_lastname, account_email, account_password)
+VALUES
+  ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
